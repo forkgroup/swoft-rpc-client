dd630a8dd8b30b7b921008028f6af4931444ba71
李铭昕 <715557344@qq.com>
2018-11-15 18:01:20 +0800

    fix: Fixed rpc client exception

--- test/server.php
+++ test/server.php
@@ -5,5 +5,6 @@ require_once __DIR__ . '/bootstrap.php';
 use Swoft\Rpc\Server\Command\RpcCommand;
 
 $command = bean(RpcCommand::class);
-
+$dir = alias('@runtime/logs');
+@mkdir($dir, 0777, true);
 $command->restart();
