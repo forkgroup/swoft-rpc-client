31272151b6de95b4d673ebf45f32b65e1774bbc5
李铭昕 <715557344@qq.com>
2018-11-15 18:14:21 +0800

    Fix

--- test/server.php
+++ test/server.php
@@ -5,6 +5,8 @@ require_once __DIR__ . '/bootstrap.php';
 use Swoft\Rpc\Server\Command\RpcCommand;
 
 $command = bean(RpcCommand::class);
+
 $dir = alias('@runtime/logs');
 @mkdir($dir, 0777, true);
+
 $command->restart();
